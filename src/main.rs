ef39bb15dab162f4fea72dd6e288f3239acb674f
Boukari Yameogo <yameogo.boukari@gmail.com>
2024-02-13 19:45:43 -0500

    working version

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,48 @@
 mod schema;
 
 use envy::from_env;
-use diesel::{Connection, Insertable, PgConnection, Queryable, RunQueryDsl, Selectable};
+use diesel::{AsChangeset, Connection, Insertable, PgConnection, Queryable, QueryDsl, RunQueryDsl, Selectable, SelectableHelper};
 use schema::users;
 use serde::{Deserialize, Serialize};
 use uuid;
 
 
 // Models definitions
-#[derive(Queryable, Deserialize, Selectable, Serialize, Debug)]
+#[derive(Queryable, Deserialize, Selectable, Serialize, AsChangeset, Debug)]
 #[diesel(table_name = users)]
 struct User {
-    id: uuid::Uuid,
+    pub id: uuid::Uuid,
     #[diesel(sql_type = diesel::sql_types::Varchar)]
-    email: String,
+    pub email: String,
     #[diesel(sql_type = diesel::sql_types::Varchar)]
-    password: String,
+    pub password: String,
     #[diesel(sql_type = diesel::sql_types::Timestamp)]
-    created_at: chrono::NaiveDateTime,
+    pub created_at: chrono::NaiveDateTime,
     #[diesel(sql_type = diesel::sql_types::Nullable<diesel::sql_types::Timestamp>)]
-    updated_at: Option<chrono::NaiveDateTime>,
+    pub updated_at: Option<chrono::NaiveDateTime>,
 }
+
+impl User {
+    fn read(conn: &mut PgConnection) -> Vec<User> {
+        users::table
+            .load::<User>(conn)
+            .expect("Error loading users")
+    }
+
+    fn update(&self, conn: &mut PgConnection) -> User {
+        diesel::update(users::table.find(self.id))
+            .set(self)
+            .get_result(conn)
+            .expect("Error updating user")
+    }
+
+    fn delete(&self, conn: &mut PgConnection) -> User {
+        diesel::delete(users::table.find(self.id))
+            .get_result(conn)
+            .expect("Error deleting user")
+    }
+}
+
 
 #[derive(Insertable, Serialize, Deserialize, Debug)]
 #[diesel(table_name = users)]
@@ -42,6 +64,14 @@ impl NewUser {
             created_at: chrono::Utc::now().naive_utc(),
         }
     }
+
+    fn create(&self, conn: &mut PgConnection) -> User {
+        diesel::insert_into(users::table)
+            .values(self)
+            .returning(User::as_returning())
+            .get_result(conn)
+            .expect("Error saving new user")
+    }
 }
 
 #[derive(Deserialize)]
@@ -54,6 +84,7 @@ fn create_connection(database_url: &str) -> PgConnection {
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
 
+
 fn main() {
     let config = from_env::<DatabaseConfig>().unwrap();
     let connection = &mut create_connection(&config.database_url);
@@ -63,11 +94,30 @@ fn main() {
         String::from("my_password"),
     );
 
+    // Create a new user
+    /*
     let created_user = diesel::insert_into(users::table)
         .values(&new_user)
         .get_result::<User>(connection)
         .expect("Error saving new user");
+    */
+    // let created_user = new_user.create(connection);
 
-    println!("{:?}", new_user);
-    println!("{:?}", created_user);
+    // Read users
+    let mut users = User::read(connection);
+    for user in &users {
+        println!("{:?}", user);
+    }
+
+    let mut _user = &mut users[0];
+     _user.password = String::from("new_password");
+    let updated_user = _user.update(connection);
+    println!("{:?}", updated_user);
+
+
+    // Update user
+
+
+    //println!("{:?}", new_user);
+    //println!("{:?}", created_user);
 }
