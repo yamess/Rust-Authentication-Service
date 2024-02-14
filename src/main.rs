24d067ecf7d2f7bab0108864887a4a7c9360f834
Boukari Yameogo <yameogo.boukari@gmail.com>
2024-02-13 23:59:45 -0500

    working crud

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 mod schema;
 
+use diesel::{
+    AsChangeset, Connection, Insertable, OptionalExtension, PgConnection, QueryDsl, Queryable,
+    RunQueryDsl, Selectable, SelectableHelper,
+};
 use envy::from_env;
-use diesel::{AsChangeset, Connection, Insertable, PgConnection, Queryable, QueryDsl, RunQueryDsl, Selectable, SelectableHelper};
 use schema::users;
 use serde::{Deserialize, Serialize};
-use uuid;
-
+use uuid::Uuid;
 
 // Models definitions
-#[derive(Queryable, Deserialize, Selectable, Serialize, AsChangeset, Debug)]
+#[derive(Queryable, Insertable, Selectable, Deserialize, Serialize, AsChangeset, Debug)]
 #[diesel(table_name = users)]
 struct User {
-    pub id: uuid::Uuid,
+    pub id: Uuid,
     #[diesel(sql_type = diesel::sql_types::Varchar)]
     pub email: String,
     #[diesel(sql_type = diesel::sql_types::Varchar)]
@@ -23,45 +25,13 @@ struct User {
 }
 
 impl User {
-    fn read(conn: &mut PgConnection) -> Vec<User> {
-        users::table
-            .load::<User>(conn)
-            .expect("Error loading users")
-    }
-
-    fn update(&self, conn: &mut PgConnection) -> User {
-        diesel::update(users::table.find(self.id))
-            .set(self)
-            .get_result(conn)
-            .expect("Error updating user")
-    }
-
-    fn delete(&self, conn: &mut PgConnection) -> User {
-        diesel::delete(users::table.find(self.id))
-            .get_result(conn)
-            .expect("Error deleting user")
-    }
-}
-
-
-#[derive(Insertable, Serialize, Deserialize, Debug)]
-#[diesel(table_name = users)]
-struct NewUser {
-    id: uuid::Uuid,
-    #[diesel(sql_type = diesel::sql_types::Varchar)]
-    email: String,
-    #[diesel(sql_type = diesel::sql_types::Varchar)]
-    password: String,
-    #[diesel(sql_type = diesel::sql_types::Timestamp)]
-    created_at: chrono::NaiveDateTime,
-}
-impl NewUser {
-    fn new(email: String, password: String) -> NewUser {
-        NewUser {
-            id: uuid::Uuid::new_v4(),
+    fn new(email: String, password: String) -> User {
+        User {
+            id: Uuid::new_v4(),
             email,
             password,
             created_at: chrono::Utc::now().naive_utc(),
+            updated_at: None,
         }
     }
 
@@ -71,6 +41,41 @@ impl NewUser {
             .returning(User::as_returning())
             .get_result(conn)
             .expect("Error saving new user")
+    }
+
+    fn find_by_id(id: &Uuid, conn: &mut PgConnection) -> Option<User> {
+        users::table
+            .find(id)
+            .first(conn)
+            .optional()
+            .expect("Error loading users")
+    }
+
+    fn get(id: &Uuid, conn: &mut PgConnection) -> Vec<User> {
+        users::table
+            .find(id)
+            .load::<User>(conn)
+            .expect("Error loading users")
+    }
+
+    fn update(&self, conn: &mut PgConnection) -> User {
+        let updated_user = User {
+            id: self.id,
+            email: self.email.clone(),
+            password: self.password.clone(),
+            created_at: self.created_at,
+            updated_at: Some(chrono::Utc::now().naive_utc()),
+        };
+        diesel::update(users::table.find(self.id))
+            .set(&updated_user)
+            .get_result(conn)
+            .expect("Error updating user")
+    }
+
+    fn delete(&self, conn: &mut PgConnection) -> usize {
+        diesel::delete(users::table.find(self.id))
+            .execute(conn)
+            .expect("Error deleting user")
     }
 }
 
@@ -84,16 +89,15 @@ fn create_connection(database_url: &str) -> PgConnection {
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
 
-
 fn main() {
     let config = from_env::<DatabaseConfig>().unwrap();
     let connection = &mut create_connection(&config.database_url);
-
-    let new_user = NewUser::new(
-        String::from("test@rust.com"),
-        String::from("my_password"),
-    );
-
+    /*
+        let new_user = User::new(
+            String::from("test@rust.com"),
+            String::from("password1234"),
+        );
+    */
     // Create a new user
     /*
     let created_user = diesel::insert_into(users::table)
@@ -101,22 +105,36 @@ fn main() {
         .get_result::<User>(connection)
         .expect("Error saving new user");
     */
-    // let created_user = new_user.create(connection);
+
+    /*
+    let created_user = new_user.create(connection);
+    println!("{:?}", created_user);
+    */
 
     // Read users
-    let mut users = User::read(connection);
-    for user in &users {
-        println!("{:?}", user);
-    }
 
+    let user = User::find_by_id(
+        &Uuid::parse_str("25c49b87-ad12-4ffe-85f3-3bad6d4f05d8").unwrap(),
+        connection,
+    );
+    match user {
+        Some(user) => println!("{:?}", user),
+        None => println!("User not found"),
+    }
+    // Update user
+    /*
     let mut _user = &mut users[0];
      _user.password = String::from("new_password");
     let updated_user = _user.update(connection);
     println!("{:?}", updated_user);
+    */
 
-
-    // Update user
-
+    // Delete User
+    /*
+    let user = &users[0];
+    let deleted_user = user.delete(connection);
+    println!("{:?}", deleted_user);
+    */
 
     //println!("{:?}", new_user);
     //println!("{:?}", created_user);
